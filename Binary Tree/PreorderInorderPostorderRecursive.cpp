aeae3c8faf5b1573aecfa65e9b0ace10719c3181
Dhananjay Daundkar <61632269+dhananjayd1729@users.noreply.github.com>
2021-05-19 11:58:59 +0530

    Create PreorderInorderPostorderRecursive.cpp

---
+++ Binary Tree/PreorderInorderPostorderRecursive.cpp
@@ -0,0 +1,67 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    
+    
+    Node(int val){
+        
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void preOrder(Node* root){
+    if (root==NULL)
+        return;
+        
+    cout<< root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void inOrder(Node* root){
+    if(root==NULL)
+       return;
+       
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+void postOrder(Node* root){
+    if(root==NULL)
+       return;
+       
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main()
+{
+   Node *root=new Node(1);
+   root->left=new Node(2);
+   root->right=new Node(3);
+   root->left->left=new Node(4);
+   root->left->right=new Node(5);
+   root->right->left=new Node(6);
+   root->right->right=new Node(7);
+   
+   cout<< "Preorder: "<<endl;
+   preOrder(root);
+   cout<< " "<<endl;
+   cout<< "Inorder: "<<endl;
+   inOrder(root);
+   cout<< " "<<endl;
+   cout<< "Postorder: "<<endl;
+   postOrder(root);
+   
+   return 0;
+}
